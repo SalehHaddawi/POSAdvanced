ff39c997878fe052a4c8eb45442136ad25843d23
mohammed Alghamdi <m5ghamdi55@gmail.com>
2018-09-20 13:57:10 +0300

    test
    hi guys

--- src/Controller/Employee_EditingController.java
+++ src/Controller/Employee_EditingController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Controller;
 
 import java.net.URL;
@@ -19,11 +14,11 @@ import javafx.scene.layout.HBox;
 
 /**
  * FXML Controller class
- *
+ * hi guys
  * @author mohamedalghamdi
  */
 public class Employee_EditingController implements Initializable {
-
+    
     @FXML
     private ImageView workerPhoto;
     @FXML
